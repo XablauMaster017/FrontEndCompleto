54d559ede4c87af8ec55527433f51fbb45610168
Xablau017 <fernandomarti017@gmail.com>
2026-03-26 23:10:39 -0300

    5 entrega fotos

---
+++ Entrega5/script.js
@@ -0,0 +1,19 @@
+//const criatura = document.getElementById("Arrombadinho");
+//cont btn = document.getElementById("comidinha");
+
+//const estados = {
+//normal: "png"
+//}
+
+//let contador = 0;
+//let intervalo = null;
+//let time_click = null;
+//let time_out = null;
+
+
+//function controlador (){
+//if(intervalo) clearInterval(intervalo);
+
+//intervalo = setInterval(()  =>{}
+
+//}
